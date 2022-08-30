f9a0e621220ffa2bd3c0f9f45037c1a9efd5a04f
webrezaul <rezaul.ctech@gmail.com>
2022-08-30 13:04:27 +0600

    blance

--- js/deposite.js
+++ js/deposite.js
@@ -1,16 +1,41 @@
-document.getElementById('btn-deposit').addEventListener('click', function () {
+
+    // Step:01
+  
+  document.getElementById('btn-deposit').addEventListener('click', function () {
 
     // console.log('button desposit clicked');
+
+    // step:2
     const depositField = document.getElementById('btn-deposit-field');
     const newDepositAmountString = depositField.value;
-    // console.log(newDepositAmountString);
-    const depositAmount = parseFloat(newDepositAmountString)
+    const newDepositAmount = parseFloat(newDepositAmountString)
+    // console.lo g(newDepositAmountString);
+
     // console.log(depositAmount)
+
+    // step:3
     depositField.value = '';
 
     // step-4
-    const depositTotalElement = document.getElementById('deposite-total');
-    const prevousDepositeTotalString = depositTotalElement.InnerText;
-    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    const depositTotalElement = document.getElementById('deposit-total');
+    const prevousDepositeTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(prevousDepositeTotalString);
+
+    // step:5
+
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = newDepositTotal;
+
+    // setep:6
+    const blanceTotalElement = document.getElementById('blance-total');
+    const blanceTotalString = blanceTotalElement.innerText;
+    const previousBlanceTotal = parseFloat (blanceTotalString);
+
+    // step:7
+
+    const newBlanceTotal = previousBlanceTotal + newDepositTotal;
+    blanceTotalElement.innerText = newBlanceTotal;
+
+
 
 })
